4f224a436f4f6810c032b963d53a8e0467883cbc
Frunzebot <ffo.ua.7@gmail.com>
2025-04-25 13:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привіт! Обери тип допису:")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
